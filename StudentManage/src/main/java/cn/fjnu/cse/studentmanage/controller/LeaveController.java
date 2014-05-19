e33bc534fb054cf99a536942680e6351d9d46c69
Seins <516912994@qq.com>
2014-05-20 00:18:49 +0800

    前端开发规则文件发布

---
+++ StudentManage/src/main/java/cn/fjnu/cse/studentmanage/controller/LeaveController.java
@@ -0,0 +1,13 @@
+package cn.fjnu.cse.studentmanage.controller;/**
+ *@类名：LeaveController
+ *@作者：Chan
+ *@创建时间：2014/5/20
+ **/
+
+/**
+ * @类名：LeaveController
+ * @作者：Chan
+ * @创建时间：2014/5/20
+ */
+public class LeaveController {
+}
