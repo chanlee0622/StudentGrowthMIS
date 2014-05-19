d9b9a2d2feca1ea02db7ca48a749b430175aa35b
Seins <516912994@qq.com>
2014-05-20 00:18:49 +0800

    初次功能提交测试

---
+++ StudentManage/src/main/java/cn/fjnu/cse/studentmanage/service/ILeaveService.java
@@ -0,0 +1,13 @@
+package cn.fjnu.cse.studentmanage.service;/**
+ *@类名：ILeaveService
+ *@作者：Chan
+ *@创建时间：2014/5/20
+ **/
+
+/**
+ * @类名：ILeaveService
+ * @作者：Chan
+ * @创建时间：2014/5/20
+ */
+public interface ILeaveService {
+}
